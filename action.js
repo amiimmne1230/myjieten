b3caadf1698d45a1cca1ca6ba8c722cb3642a623
am <amiimmne1230@gmail.com>
2021-09-02 13:11:17 +0800

    first commit

--- action.js
+++ action.js
@@ -4,14 +4,13 @@ var searchhtml = document.getElementById("actionwork");
 var needcd = 0;
 //行動CD
 
-
 //Math.round(Math.max(1, Math.random()*5))
 //亂數設定
 var intervalID;
 //計時器
 
-var bool_checkinaction = false;
-//BOOL 確認是否在行動中
+
+
 
 
 
@@ -20,10 +19,12 @@ function goindex(ass)
 	var goindex0 = document.getElementById("goindex0");//櫛田
 	var goindex1 = document.getElementById("goindex1");//物品
 	//var goindex2 = document.getElementById("goindex2");//
-
+	//目錄定義
 
 	goindex0.style.display = "none";
 	goindex1.style.display = "none";
+	//先關
+
 	switch(ass)
 	{
 		case 0:
@@ -33,6 +34,7 @@ function goindex(ass)
 		goindex1.style.display =	"";		
 		break;	
 	}
+	//在打開
 }
 //目錄開關
 
@@ -42,87 +44,107 @@ function iii(m,x)
 {
 	return Math.round(Math.max(m, Math.random()*x));
 }
+//方便取得亂數  iii(,);
+
+
+
 function disallbutppn(ass,ass2)
 {
-	bool_checkinaction = true;
 	searchbutton0.disabled = true;
 	searchbutton1.disabled = true;
 	searchbutton2.disabled = true;
 	searchbutton3.disabled = true;
 	searchbutton4.disabled = true;
 	searchbutton5.disabled = true;
+	//行動按鈕全觀
 
+	searchhtml.innerHTML = ass;	//指定內榮
+	var aasdasd = 100/needcd;//CD計算 %數
 
-	searchhtml.innerHTML = ass;	
-	var aasdasd = 100/needcd;
 	intervalID = setInterval(function()
-	{//needcd -= 999999;//測試
+	{
+		//needcd -= 999999;//測試
 		needcd--; //正常		
-		doload(aasdasd);
+		doload(aasdasd);//進度條
 		if(needcd <= 0)
-		{
+		{	
+			clearInterval(intervalID);
 			actionload.style.backgroundColor =	"lightgreen";
-			if(ass2 == 0)
-				{clearInterval(intervalID);
+			switch(ass2)
+			{
+				case 0:
 					var a = iii(1,3);
 					searchhtml.innerHTML = '自習完成 得到 ' + a + ' 點學力  體力消耗 ' +  2 + ' 點';				
 					arca+=a;
 					state0.innerHTML = arca;
 					powerevent(-2);
-				}
-			if(ass2 == 1)
-				{clearInterval(intervalID);
+				break;
+				//自習
+
+
+
+				case 1:
 					var a = iii(1,6);
 					searchhtml.innerHTML = '鍛鍊完成 得到 ' + a + ' 點身體能力  體力消耗 ' +  3 + ' 點';
 					body+=a;
 					state1.innerHTML = body;
 					powerevent(-3);
-				}
-			if(ass2 == 2)
-				{clearInterval(intervalID);
+				break;
+				//鍛鍊
+
+
+
+				case 2:
 					searchhtml.innerHTML = '休息完了 體力恢復 ' + 10 + ' 點';	
 					powerevent(10);	
-				}
-				if(ass2 == 3)
-				{
-					clearInterval(intervalID);
-					var a = iii(0,15);
-					var b = iii(0,4);
-					searchhtml.innerHTML = '下課了 得到 ' + a + ' 點學力, ' + b + ' 點思考力 體力消耗 ' +  8 + ' 點';
+				break;
+				//睡覺
+
+
+				case 3:
+					var a = iii(4,16);
+					var b = iii(0,3);
+					searchhtml.innerHTML = '下課了 得到 ' + a + ' 點學力 和 ' + b + ' 點思考力 體力消耗 ' +  9 + ' 點';
 					arca+=a;
 					think+=b;					
 					state0.innerHTML = arca;
 					state2.innerHTML = think;
-					powerevent(-8);
-				}
-				if(ass2 == 4)
-				{	
-					clearInterval(intervalID);
+					powerevent(-9);
+				break;
+				//上課
+
+
+				case 4:
 					var aa =0;
-					var asd = iii(3,6);
-					if(iii(0,22) == 0)
+					var asd = iii(2,6);
+					if(iii(0,18) == 0)
 						aa++;
 					searchhtml.innerHTML = '交流完成 交到 ' + aa + ' 位朋友  體力消耗 ' + asd  + ' 點';					
 					frien+= aa;
 					state6.innerHTML = frien;
 					powerevent(-asd);
-				}
-				if(ass2 == 5)
-				{
-					clearInterval(intervalID);
+				break;
+				//交朋友
+
+
+				case 5:
 					searchhtml.innerHTML = '買到炒麵麵包了   Pr ' + -20 + ' 點  體力消耗 ' + 2 +" 點";		
 					item[0]++;
 					item00.innerHTML = item[0];
 					powerevent(-2);
 					prevent(-20);
-				}
-				
-					timeoutID = setTimeout(Doneactioneff, 3000);
+				break;
+				//買麵包
+			}
+			timeoutID = setTimeout(Doneactioneff, 3000);
 		}
-		
-
 	},1000);
 }
+//行動事件用
+
+
+
+
 function powerevent(ass)
 {
 	nowpower+= ass;
@@ -131,18 +153,20 @@ function powerevent(ass)
 	if(nowpower < 0)
 		nowpower = 0;
 	state3.innerHTML = nowpower + "/" + maxpower;
-}
+}// 體力 計算
 function prevent(ass)
 {
 	cococo+= ass;	
 	state7.innerHTML = cococo;
-}
+}// Pr 計算
+
+
 
 function Doneactioneff() 
 {	
-	overload();
-	bool_checkinaction = false;
-	clearTimeout(timeoutID);
+	overload();//
+	clearTimeout(timeoutID);//停止 文字顯示
+
     searchhtml.innerHTML = '...';
 	searchbutton0.disabled = false;
 	searchbutton1.disabled = false;
@@ -150,8 +174,11 @@ function Doneactioneff()
 	searchbutton3.disabled = false;
 	searchbutton4.disabled = false;
 	searchbutton5.disabled = false;
+	//以上是行動按鈕
 }
-//按鈕切換
+//行動完成後 所有東西還原
+
+
 
 
 function clearall(ass,ass2)
@@ -159,31 +186,50 @@ function clearall(ass,ass2)
 	searcharea0.disabled = false;
 	searcharea1.disabled = false;
 	searcharea2.disabled = false;
+	//地點
+
 	a0.style.display = "none";
 	a1.style.display = "none";
 	a2.style.display = "none";
+	//行動
+
 	ass.disabled = true;
 	ass2.style.display = "";
 }
+//切換地點的按鈕與行動按鈕
+
+
+
+
+
+
 var a0 = document.getElementById("actiondiv0");
 var a1 = document.getElementById("actiondiv1");
 var a2 = document.getElementById("actiondiv2");
-
-
+//哪個地點的行動定義
 var searcharea0 = document.getElementById("area0");
+var searcharea1 = document.getElementById("area1");
+var searcharea2 = document.getElementById("area2");
+//地點定義
+var searchbutton0 = document.getElementById("button0");
+var searchbutton1 = document.getElementById("button1");
+var searchbutton2 = document.getElementById("button2");
+var searchbutton3 = document.getElementById("button10");
+var searchbutton4 = document.getElementById("button11");
+var searchbutton5 = document.getElementById("button20");
+//行動定義
+
+
+
 searcharea0.addEventListener("click",function()
 {	
 	clearall(searcharea0,a0);
 });
 //地點 宿舍
-
-var searcharea1 = document.getElementById("area1");
 searcharea1.addEventListener("click",function()
 {	clearall(searcharea1,a1);
 });
 //地點 宿舍
-
-var searcharea2 = document.getElementById("area2");
 searcharea2.addEventListener("click",function()
 {	
 clearall(searcharea2,a2);
@@ -195,12 +241,9 @@ clearall(searcharea2,a2);
 
 
 
-
-
-
-var searchbutton0 = document.getElementById("button0");
 searchbutton0.addEventListener("click",function()
-{	if(boolhavepow(2))
+{	
+	if(boolhavepow(2))
 	{
 	needcd = 3;
 	disallbutppn('你把之前上課的內容再溫習一次',0);
@@ -209,9 +252,6 @@ searchbutton0.addEventListener("click",function()
 	actionfail();
 });
 //自習
-
-
-var searchbutton1 = document.getElementById("button1");
 searchbutton1.addEventListener("click",function()
 {	if(boolhavepow(3))
 	{
@@ -221,19 +261,12 @@ searchbutton1.addEventListener("click",function()
 	actionfail();
 });
 //自我鍛鍊
-
-
-var searchbutton2 = document.getElementById("button2");
 searchbutton2.addEventListener("click",function()
 {	
 	needcd = 177;
 	disallbutppn('你小歇一會',2);	
 });
 //休息
-
-
-
-var searchbutton3 = document.getElementById("button10");
 searchbutton3.addEventListener("click",function()
 {	
 	if(boolhavepow(8))
@@ -245,10 +278,6 @@ searchbutton3.addEventListener("click",function()
 	actionfail();
 });
 //上課
-
-
-
-var searchbutton4 = document.getElementById("button11");
 searchbutton4.addEventListener("click",function()
 {	
 	if(boolhavepow(3))
@@ -260,10 +289,6 @@ searchbutton4.addEventListener("click",function()
 	actionfail();
 });
 //交朋友
-
-
-
-var searchbutton5 = document.getElementById("button20");
 searchbutton5.addEventListener("click",function()
 {	
 	if(boolhavepr(20) && boolhavepow(2))
@@ -276,11 +301,14 @@ searchbutton5.addEventListener("click",function()
 });
 //買超面面標
 
+
+
+
 function actionfail()
 {
 	searchhtml.innerHTML = '你沒有足夠的  Pr 或 體力'
-		timeoutID = setTimeout(Doneactioneff, 1500);
-}
+	timeoutID = setTimeout(Doneactioneff, 1500);
+}//沒有PR 或 體力
 function boolhavepr(ass)
 {
 	if(cococo < ass)
--- playerStat.js
+++ playerStat.js
@@ -1,28 +1,33 @@
-﻿
-
-var		arca=8;//學歷
+﻿var		arca=8;//學歷
 var		body=8;//身體能力
 var		think=7;//思考力
 var		nowpower=30;//體力
 var		maxpower=30;//最大體
-var		soci=12;////社交
+var		soci=12;////團體合作
 var		frien=0;//朋友
-var		cococo=50;//前
-var		boing=1;//身材
+var		cococo=50;//PR
 
 
 var		item = [
-0,//炒麵麵包
-0 //山內同學的聯絡方式
+0,//炒麵麵包 0
+0, //山內同學的聯絡方式 1
+0,//歷年考試卷 2
+0//原規 3
 ]
 //物品
 
 
-var	stat =	document.getElementById("statname");//設定狀態
 
 
-var		playerstat= ["活著","活著(黑化)","亡矣"];
+
+
+var	stat =	document.getElementById("statname");
+//玩家狀態 文字
+var	playerstat= ["活著","活著(黑化)","亡矣"];
 				//狀態 0      1          2
+
+
+
 
 var state0 = document.getElementById("arcade"),
 	state1 = document.getElementById("body"),
@@ -31,39 +36,68 @@ var state0 = document.getElementById("arcade"),
 	state5 = document.getElementById("social"),
 	state6 = document.getElementById("friends"),
 	state7 = document.getElementById("coco"),
-	state8 = document.getElementById("boingboing"),
-	item00 = document.getElementById("item00")
-	item01 = document.getElementById("item01")
+
+	item00 = document.getElementById("item00"),
+	item01 = document.getElementById("item01"),
+	item02 = document.getElementById("item02"),
+	item03 = document.getElementById("item03")
 	;
 	//必須依各個設定
 	
 	
 	
-	startallset();
+	
+	
+
+
+
+startallset();//一打開會先執行這個
+
+
 function startallset()
 {	
-		if( state0 != null)
-		state0.innerHTML = arca;
+		if(state0 != null)
+			state0.innerHTML = arca;
+
 		if(state1 != null)
-		state1.innerHTML = body;
+			state1.innerHTML = body;
+
 		if(state2 != null)
-		state2.innerHTML = think;
+			state2.innerHTML = think;
+
 		if(state3 != null)
-		state3.innerHTML = nowpower + "/" + maxpower;
+			state3.innerHTML = nowpower + "/" + maxpower;
+
 		if(state5 != null)
-		state5.innerHTML = soci;
+			state5.innerHTML = soci;
+
 		if(state6 != null)
-		state6.innerHTML = frien;
+			state6.innerHTML = frien;
+
 		if(state7 != null)
-		state7.innerHTML = cococo;
-		if(state8 != null)
-		state8.innerHTML = boing;
+			state7.innerHTML = cococo;
+		
+
+
 		if(stat != null)
-		stat.innerHTML = playerstat[0];//一開始活者?
+			stat.innerHTML = playerstat[0];//一開始活者?
+
+
+
 		if(item00 != null)
-		item00.innerHTML = item[0];
+			item00.innerHTML = item[0];
+
 		if(item01 != null)
-		item01.innerHTML = item[1];
+			item01.innerHTML = item[1];
+
+		if(item02 != null)
+			item02.innerHTML = item[2];
+
+		if(item03 != null)
+			item03.innerHTML = item[3];
+
+
+
 }//一開始設定
 
 
